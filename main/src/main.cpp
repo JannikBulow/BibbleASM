a86d33002b8f3ed7dada80d2c8dfef193c46bae7
JesusTouchMe <dodoakmishot@gmail.com>
2026-05-07 22:48:58 +0200

    feat(main): add full assembler pipeline with new parser

--- main/src/main.cpp
+++ main/src/main.cpp
@@ -2,8 +2,13 @@
 
 #include "BibbleASM/lexer/lexer.h"
 
+#include "BibbleASM/parser/parser.h"
+
+#include <BibbleBytecode/writer.h>
+
 #include <fstream>
 #include <iostream>
+#include <filesystem>
 #include <sstream>
 
 int main(int argc, char** argv) {
@@ -25,9 +30,21 @@ int main(int argc, char** argv) {
     bibbleasm::Lexer lexer(text);
     std::vector<bibbleasm::Token> tokens = lexer.lex();
 
-    for (const auto& token : tokens) {
-        std::cout << token << std::endl;
+    bibbleasm::Parser parser(argv[1], tokens);
+
+    bibbleasm::ModuleBuilder moduleBuilder = parser.parse();
+    bibbleasm::Module module = moduleBuilder.build();
+
+    bibblebytecode::WritableByteBuffer buf;
+    if (!bibblebytecode::writer::WriteModule(buf, module.module())) {
+        return 70;
     }
+
+    std::filesystem::path outputFile = argv[1];
+    outputFile.replace_extension("bmod");
+
+    std::ofstream output(outputFile, std::ios::binary);
+    buf.emit(output);
 
     return 0;
 }
